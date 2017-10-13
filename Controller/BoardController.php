f12d80376fce9e6c6c5978914b88df9e02eae48f
Luke Bainbridge <luke.bainbridge@gmail.com>
2017-10-13 19:42:59 -0400

    Adding editorconfig and proper template.

--- Controller/BoardController.php
+++ Controller/BoardController.php
@@ -2,12 +2,33 @@
 
 namespace Bkstg\NoticeBoardBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Bkstg\CoreBundle\Controller\Controller;
+use Bkstg\CoreBundle\Entity\Production;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class BoardController extends Controller
 {
-    public function showAction($production_slug)
-    {
-        return $this->render('@BkstgNoticeBoard/Board/show.html.twig');
+    public function showAction(
+        $production_slug,
+        AuthorizationCheckerInterface $auth
+    ) {
+        // Lookup the production by production_slug.
+        $production_repo = $this->em->getRepository(Production::class);
+        if (null === $production = $production_repo->findOneBy(['slug' => $production_slug])) {
+            throw new NotFoundHttpException();
+        }
+
+        // Check permissions for this action.
+        if (!$auth->isGranted('GROUP_ROLE_USER', $production)) {
+            throw new AccessDeniedException();
+        }
+
+        // Return response.
+        return new Response($this->templating->render('@BkstgNoticeBoard/Board/show.html.twig', [
+            'production' => $production,
+        ]));
     }
 }
