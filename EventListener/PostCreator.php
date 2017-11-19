5d66ac2d4e7b36dbcf933b0d585bf0c44cc03a77
Luke Bainbridge <luke.bainbridge@gmail.com>
2017-11-19 07:35:31 -0500

    Using new user provider and link for actions.

--- EventListener/PostCreator.php
+++ EventListener/PostCreator.php
@@ -6,18 +6,22 @@ use Bkstg\CoreBundle\User\UserProviderInterface;
 use Bkstg\NoticeBoardBundle\Entity\Post;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Spy\Timeline\Driver\ActionManagerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class PostCreator
 {
     private $action_manager;
     private $user_provider;
+    private $url_genertor;
 
     public function __construct(
         ActionManagerInterface $action_manager,
-        UserProviderInterface $user_provider
+        UserProviderInterface $user_provider,
+        UrlGeneratorInterface $url_generator
     ) {
         $this->action_manager = $action_manager;
         $this->user_provider = $user_provider;
+        $this->url_generator = $url_generator;
     }
 
     public function postPersist(LifecycleEventArgs $args)
@@ -34,6 +38,13 @@ class PostCreator
         foreach ($post->getGroups() as $group) {
             $group_component = $this->action_manager->findOrCreateComponent($group);
             $action = $this->action_manager->create($user_component, 'post', array('directComplement' => $group_component));
+            $action->setLink($this->url_generator->generate(
+                'bkstg_board_show',
+                [
+                    '_fragment' => 'post-'.$post->getId(),
+                    'production_slug' => $group->getSlug()
+                ]
+            ));
             $this->action_manager->updateAction($action);
         }
     }
